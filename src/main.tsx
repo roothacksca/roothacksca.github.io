ced04cb8cc5e7663ad8abb673257f97df8a9690e
Fwuffyy <fwuffy@fwuffy.tk>
2024-06-09 10:26:49 -0400

    init react source

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("react-mount")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
